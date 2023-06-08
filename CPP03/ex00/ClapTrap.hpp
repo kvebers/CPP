83d755f3334e34dea3653edf0ef5f7c54f00653a
kvebers <kvebers@student.42heilbronn.de>
2023-06-08 13:52:14 +0200

    update

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 12:14:17 by kvebers           #+#    #+#             */
-/*   Updated: 2023/06/08 13:14:43 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/06/08 13:36:55 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,24 @@ private:
     int _energyPoints;
     int _attackDamage;
 public:
+    //orthodox form
     ClapTrap(const std::string name);
     ~ClapTrap();
     ClapTrap(const ClapTrap &trap);
     ClapTrap &operator=(const ClapTrap &trap);
+    //functions needed to implement by subject
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
+    //setters and getters
+    void setName(std::string name);
+    void setHealth(int health);
+    void setEnergy(int energy);
+    void setDamage(int damage);
+    std::string getName(void);
+    int getHealth(void);
+    int getDamage(void);
+    int setEnergy(void);
 };
 
 #endif
