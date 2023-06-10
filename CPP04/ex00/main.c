2b911571699de7fe41b8e7eafa683400ccf6bde2
kvebers <kvebers@student.42heilbronn.de>
2023-06-10 18:57:21 +0200

    Started work on CPP04

---
+++ CPP04/ex00/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/10 13:28:12 by kvebers           #+#    #+#             */
+/*   Updated: 2023/06/10 17:37:04 by kvebers          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+	{
+		const Animal* meta = new Animal();
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound(); //will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
+	}
+}
