71fe170936c58b894025e392eff4638210c6176f
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-01 14:40:06 +0200

    Rewriting ex00 CPP06

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 11:01:20 by kvebers           #+#    #+#             */
-/*   Updated: 2023/07/10 17:04:20 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/08/01 14:36:07 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,51 +34,50 @@ ScalarConverter::ScalarConverter(const ScalarConverter &idk) {
 	std::cout << "IDK wHY WE NEED THIS IN THIS EXECERSISE" << std::endl;
 }
 
+// MY AMAZING CUSTOM EXCEPTION FUNCTION
+const char *ScalarConverter::CustomException::what() const throw() { return message.c_str(); }
+ScalarConverter::CustomException::CustomException(const std::string &word) : message(word) {}
+ScalarConverter::CustomException::~CustomException() throw() {}
+
 // OTHER STUFF
 
 void ScalarConverter::convertChar(const std::string &input) {
-	try
-	{
-	if (input.length() == 1) {
-		if (std::isprint(input[0]))
-			std::cout << "char: " << input[0] << std::endl;
-		else
-			std::cout << "char: impossible " << std::endl;
-	} else {
-		std::cout << "char: impossible" << std::endl;
+	try {
+		if (input.length() == 1) {
+			if (std::isprint(input[0]))
+				std::cout << "char: " << input[0] << std::endl;
+			else
+				std::cout << "char: impossible " << std::endl;
+		} else {
+			throw CustomException("Char ain't possible");
+		}
+	} catch (const std::exception &e) {
 	}
-	} catch (const std::exception &e) {}
 }
 
 void ScalarConverter::convertDouble(const std::string &input) {
-	try {
-		std::cout << "double: " << std::fixed << std::setprecision(1)
-				  << static_cast<double>(std::stod(input)) << std::endl;
-	} catch (const std::exception &e) {
-		std::cout << "double: impossible " << std::endl;
-	}
+	std::cout << "double: " << std::fixed << std::setprecision(1)
+			  << static_cast<double>(std::stod(input)) << std::endl;
 }
 
 void ScalarConverter::convertFloat(const std::string &input) {
-	try {
-		std::cout << "float: " << std::fixed << std::setprecision(1)
-				  << static_cast<float>(std::stof(input)) << "f" << std::endl;
-	} catch (const std::exception &e) {
-		std::cout << "float: impossible " << std::endl;
-	}
+	std::cout << "float: " << std::fixed << std::setprecision(1)
+			  << static_cast<float>(std::stof(input)) << "f" << std::endl;
 }
 
 void ScalarConverter::convertInt(const std::string &input) {
-	try {
-		std::cout << "int: " << static_cast<int>(std::stoi(input)) << std::endl;
-	} catch (const std::exception &e) {
-		std::cout << "int: impossible " << std::endl;
-	}
+	std::cout << "int: " << static_cast<int>(std::stoi(input)) << std::endl;
 }
 
-
-
 void ScalarConverter::convert(const std::string &input) {
-	if (std::strlen(input) == 1)
-		return std::literals
+	try {convertChar(input);} catch (const std::exception &e) {
+		try { convertDouble(input); } catch (const std::exception &e) {
+			try { convertFloat(input); } catch (const std::exception &e) {
+				try { convertInt(input); } catch (const std::exception &e) {
+		
+
+				}
+			}
+		}
+	}
 }
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 21:23:49 by kvebers           #+#    #+#             */
-/*   Updated: 2023/06/30 11:55:21 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/08/01 14:23:47 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,22 @@ class ScalarConverter {
 	ScalarConverter &operator=(const ScalarConverter &idk);
 	ScalarConverter(const ScalarConverter &idk);
 	static void convert(const std::string &input);
+
    private:
-   static void convertChar(const std::string &input);
-   static void convertDouble(const std::string &input);
-   static void convertInt(const std::string &inpit);
-   static void convertFloat(const std::string &input);
+	static void convertChar(const std::string &input);
+	static void convertDouble(const std::string &input);
+	static void convertInt(const std::string &inpit);
+	static void convertFloat(const std::string &input);
+
+	class CustomException : public std::exception {
+	   private:
+		std::string message;
+
+	   public:
+		CustomException(const std::string &word);
+		const char *what() const throw();
+		virtual ~CustomException() throw();
+	};
 };
 
 #endif
