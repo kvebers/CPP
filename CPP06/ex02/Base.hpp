47141945f67fcfaa5ef5988e733d7a4b5e7caf42
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-02 13:34:06 +0200

    Shortened

--- CPP06/ex02/Base.hpp
+++ CPP06/ex02/Base.hpp
@@ -8,4 +8,14 @@ class Base {
 	virtual ~Base();
 };
 
+class A : public Base {
+	~A() { std::cout << "JUST FOR THE TEST UNCOMMENT" << std::endl; }
+};
+class B : public Base {
+	~B() { std::cout << "JUST FOR THE TEST UNCOMMENT" << std::endl; }
+};
+class C : public Base {
+	~C() { std::cout << "JUST FOR THE TEST UNCOMMENT" << std::endl; }
+};
+
 #endif
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -1,6 +1,25 @@
+
 #include "Base.hpp"
 
-int main()
-{
-    
+Base *generate(void) {
+	srand(time(0));
+	int switcher = rand() % 3;
+	if (switcher == 0)
+		return new A;
+	else if (switcher == 1)
+		return new B;
+	else if (switcher == 2)
+		return new C;
+	return (NULL);
+}
+
+void identify(Base *p) {}
+
+void identify(Base &p) {}
+
+int main() {
+	Base *test = generate();
+	identify(test);
+	identify(*test);
+	delete test;
 }
