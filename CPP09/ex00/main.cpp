30e6fc1a33f797d20ab177bd2b0887ed48a8b35d
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-06 12:27:27 +0200

    CPP09

---
+++ CPP09/ex00/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+
+void fillTheMap()
+{
+	
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2) std::cerr << "Wrong number of arguments " << std::endl;
+	std::map<std::string, double> bitcoinMap;
+	try {
+	} catch (std::exception &e) {
+	}
+}
