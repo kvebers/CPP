6ac6384761add33aad8ff5ce5e7bf9f0e89f6ec7
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-08 15:15:55 +0200

    Template

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,21 +6,46 @@
 #include <list>
 #include <vector>
 
-template <typename CONT>
-void insertionSort(CONT &cont, typename CONT::iterator left, typename CONT::iterator right) {}
+#define TRESHOLD 10
+
+
+
+
 
 template <typename CONT>
-void merge(CONT &cont, typename CONT::iterator left, typename CONT::iterator mid,
-		   typename CONT::iterator right) {}
+void insertionSort(CONT &cont, typename CONT::iterator start, typename CONT::iterator end) {
+
+}
 
 template <typename CONT>
-void fordJohnsonSort(CONT &cont, typename CONT::iterator left, typename CONT::iterator right) {
-	if (std::distance(left, right) > 0) {
-	
-	
-	
-	
-	
+void merge(CONT &cont, typename CONT::iterator start, typename CONT::iterator mid,
+		   typename CONT::iterator end) {
+	for (typename CONT::iterator cnt = start + 1; cnt != end; cnt++) {
+		typename CONT::value_type theValue = *cnt;
+		typename CONT::iterator previousCnt = cnt - 1;
+		while (previousCnt >= start && theValue < *previousCnt)	 // Perform Shift
+		{
+			*(previousCnt + 1) = *previousCnt;
+			previousCnt--;
+		}
+		*(previousCnt + 1) = theValue;
+	}
+}
+template <typename CONT>
+void recursion(CONT &cont, typename CONT::iterator start, typename CONT::iterator end) {
+	typename CONT::iterator mid = start + std::distance(start, end) / 2;
+	fordJohnsonSort(cont, start, mid);
+	fordJohnsonSort(cont, mid + 1, end);
+	merge(cont, start, mid, end);
+}
+
+template <typename CONT>
+void fordJohnsonSort(CONT &cont, typename CONT::iterator start, typename CONT::iterator end) {
+	if (std::distance(start, end) > 0) {
+		if (std::distance(start, end) < TRESHOLD)
+			insertionSort(cont, start, end);
+		else
+			recursion(cont, start, end);
 	}
 }
 
