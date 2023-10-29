d8c5ab6afbd309b96de5ae3e816aab03ee2f41c8
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-10-29 13:01:10 +0100

    Small changes in readability

--- CPP_EXAM/ex02/BrickWall.hpp
+++ CPP_EXAM/ex02/BrickWall.hpp
@@ -1,15 +1,14 @@
 #ifndef BRICKWALL_HPP
-	#define BRICKWALL_HPP
+#define BRICKWALL_HPP
 
 #include "ATarget.hpp"
 
-class BrickWall : public ATarget
-{
-public:
+class BrickWall : public ATarget {
+   public:
 	BrickWall();
 	~BrickWall();
 
-	virtual ATarget * clone() const;
+	virtual ATarget* clone() const;
 };
 
 #endif
